d16d3f1ad4364a8c6e061604db087dfaf1d60f9d
deworld <deworldgreen@gmail.com>
2022-03-23 21:19:55 +0300

    fix4
    fix3

---
+++ Shops/Entities/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace Shops.Entities
+{
+    public class Person
+    {
+        private string _name;
+        private double _money;
+
+        public Person(string name, double moneyBefore)
+        {
+            _name = name;
+            _money = moneyBefore;
+        }
+
+        public void Withdrawal(double money)
+        {
+            _money -= money;
+        }
+
+        public double GetMoney()
+        {
+            return _money;
+        }
+    }
+}
